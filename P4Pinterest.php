ce7eb6a1efd7c367a5b280c03602b214129bd83a
KJayce <jaycekaje@gmail.com>
2017-06-01 11:46:18 +0200

    second wind

--- P4Pinterest.php
+++ P4Pinterest.php
@@ -1,5 +1,9 @@
 <?php
-
+$_FILES['pik']['name'] //basic name of the file, just like on sender's HD ex: icon_name.jpg
+$_FILES['pik']['type'] //type of the file ex: image/png.
+$_FILES['pik']['size'] //files  size in bytes
+$_FILES['pik']['tmp_name'] //uploaded file's  address temporary directory
+$_FILES['pik']['error'] // error code, make it possible to see if file has been uploaded
 
 
 ?>
