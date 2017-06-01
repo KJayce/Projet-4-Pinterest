3ff1ce64fcf29cf3b5f30c05d111c256b3e65894
KJayce <jaycekaje@gmail.com>
2017-06-01 15:13:35 +0200

    ???

--- P4Pinterest.php
+++ P4Pinterest.php
@@ -4,4 +4,7 @@ $_FILES['pik']['type'] //type of the file ex: image/png.
 $_FILES['pik']['size'] //files  size in bytes
 $_FILES['pik']['tmp_name'] //uploaded file's  address temporary directory
 $_FILES['pik']['error'] // error code, make it possible to see if file has been uploaded
+
+
+
 ?>
