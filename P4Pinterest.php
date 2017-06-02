1534f611f9121687fca3cba4c46f080989b35f41
KJayce <jaycekaje@gmail.com>
2017-06-02 12:40:07 +0200

    ???é

--- P4Pinterest.php
+++ P4Pinterest.php
@@ -1,14 +1,3 @@
 <?php
-$_FILES['pik']['name'] //basic name of the file, just like on sender's HD ex: icon_name.jpg
-$_FILES['pik']['type'] //type of the file ex: image/png.
-$_FILES['pik']['size'] //files  size in bytes
-$_FILES['pik']['tmp_name'] //uploaded file's  address temporary directory
-$_FILES['pik']['error'] // error code, make it possible to see if file has been uploaded
 
-
-if(!empty($_FILES)){
-    $message = "_FILES EST REMPLI";
-    $image_name = $_FILES ["image_1"]["name"];
-    $_FILES ["image_1"]["type"];
-}
 ?>
