d9e7b451229a24ff5b5271fe78dc7614e5591d67
KJayce <jaycekaje@gmail.com>
2017-07-05 15:04:05 +0200

    starting over

--- P4Pinterest.php
+++ P4Pinterest.php
@@ -1,3 +1,30 @@
 <?php
+$upDir = "./assets/uploads"
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="post" action="P4Pinterest.php" enctype="multipart/form-data">
+
+        <label for="pik">Image to upload :</label><br />
+        <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+        <input type="file" name="pik" id="pik" /><br />
+
+        <label for="description">File description :</label><br />
+        <textarea name="description" id="description"></textarea><br />
+        <button type="submit"> Upload File </button>
+
+    </form>
+
+</body>
+</html>
