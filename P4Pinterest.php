b14d329c98eda8204eb8122b5a91070b40bd18ff
KJayce <jaycekaje@gmail.com>
2017-07-06 16:26:38 +0200

    j

--- P4Pinterest.php
+++ P4Pinterest.php
@@ -1,31 +1,42 @@
 <?php
+$upDir = "assets/uploads/";
+
 if(isset($_POST["submit"])){
-    $err = array();
-    $Fname = $_FILES["pik"]["name"];
-    $Fsize = $_FILES["pik"]["type"];
-    $Ftmp = $_FILES["pik"]["tmp_name"];
-    $Ftype = $_FILES["pik"]["type"];
-    $Fext = strtolower(end(explode('.', $_FILES["image"]["name"])));
+    $fName = $_FILES["pik"]["name"];
+    $fTarget = $upDir . basename($Fname);
+    $fType = pathinfo($fTarget, PATHINFO_EXTENSION);
+}
+$err = false;
 
-    $ext = array("jpeg", "jpg", "png");
-
-    if(in_array($Fext, $ext) === false){
-        $err[] = "extension not allowed, please choose a JPEG, JPG or PNG file.";
-    }
-
-    if($Fsize > 4097152){
-        $err = "file exceeding size limit, please choose a file of 4MB or less.";
-    }
-
-    if(empty($err)== true){
-        move_uploaded_file($Ftmp,"assets/uploads/".$Fname);
-        echo "File upload was successful!";
-    }else{
-        print_r($err);
+if(isset($_POST["submit"])){
+    $fNametmp = $_FILES["pik"]["tmp_name"];
+    $check = getimagesize($fNametmp);
+    if($check !== false){
+        echo "<p> The file you uploaded is an image - " . $check ["mime"] . " .</p>";
+        $err = false;
+    }else {
+        echo "<p> Sorry, you must upload an images only.</p>";
+        $err = true;
     }
 }
 
+if($_FILES["fName"]["size"] > 500000){
+    echo "<p> File's size exceeds authorized limit </p>";
+    $err;
+}
 
+if($fType !== "jpg" &&
+   $fType !== "png" && 
+   $fType !== "jpeg" && 
+   $fType !== "gif" && 
+   $fType !== "svg"){
+    echo "<p> Please upload an image in the following format: jpg, png, jpeg, gif, svg </p>";
+    $err;
+}
+
+if ($err){
+    echo "<p> File could not be uploaded </P>";
+}
 ?>
 
 
@@ -49,7 +60,7 @@ if(isset($_POST["submit"])){
         <button type="submit"> Upload File </button>
 
         <ul>
-            <li>Sent file: <?php echo $_FILES["pik"]["name"] ?>²</li>
+            <li>Sent file: <?php echo $_FILES["pik"]["name"] ?></li>
             <li>File Size: <?php echo $_FILES["pik"]["size"] ?></li>
             <li>File type: <?php echo $_FILES["pik"]["type"] ?></li>
         </ul>
